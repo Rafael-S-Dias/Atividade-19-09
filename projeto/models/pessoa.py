12414327bf61ce93f5b23f0593ea4e013de971d3
Rafael-S-Dias <161033176+Rafael-S-Dias@users.noreply.github.com>
2024-09-26 14:27:20 +0000

    Salvar

--- projeto/models/pessoa.py
+++ projeto/models/pessoa.py
@@ -2,15 +2,39 @@ from projeto.models.enums.sexo import Sexo
 
 class Pessoa:
     def __init__(self, nome : str, idade: int, sexo: Sexo) -> None:
-        self._nome = nome
-        self._idade = idade
+        self._nome = self._verificar_nome(nome)
+        self._idade = self._verificar_idade(idade)
         self._sexo = sexo
+
+    def _verificar_nome(self, valor):
+        self._verificar_nome_tipo_invalido(valor)
+
+        self._nome = valor
+        return self._nome 
     
-    def _set_idade(self, idade):
-        if idade < 0:
-            self._idade = 0
-        else: 
-            self._idade = idade
+    def _verificar_idade(self, valor):
+        self._verificar_idade_tipo_invalido(valor)
+        self._verificar_idade_negativa(valor)
+        self._verificar_idade_maior_130(valor)
+
+        self._idade = valor
+        return self._idade 
+
+    def _verificar_nome_tipo_invalido(self, valor):
+        if not isinstance(valor, str):
+            raise TypeError("O nome deve ser um texto!")
+        
+    def _verificar_idade_tipo_invalido (self, valor):
+        if not isinstance(valor, int):
+            raise TypeError("A idade deve ser número inteiro!")
+
+    def _verificar_idade_negativa (self, valor):
+        if valor < 0:
+            raise ValueError("A idade não pode ser negativa!")
+
+    def _verificar_idade_maior_130 (self, valor): 
+        if valor > 130:
+            raise ValueError("A idade não pode ser maior que 130 anos!")
 
     def __str__(self) -> str:
         return(
--- projeto/tests/test_pessoa.py
+++ projeto/tests/test_pessoa.py
@@ -12,7 +12,22 @@ def test_pessoa_nome_valido(pessoa_valida):
 
 def test_pessoa_idade_valida(pessoa_valida):   
     assert pessoa_valida._idade == 23
+
+def test_pessoa_nome_tipo_invalido_mostrar_mensagem():  
+    with pytest.raises(TypeError, match = "O nome deve ser um texto!"):
+        Pessoa("111", 23, Sexo.MASCULINO)
+
+def test_pessoa_idade_tipo_invalido_mostrar_mensagem():  
+    with pytest.raises(TypeError, match = "A idade deve ser número inteiro!"):
+        Pessoa("Fuboca", "23", Sexo.MASCULINO)
+
+def test_pessoa_idade_negativa_mostrar_mensagem():  
+    with pytest.raises(ValueError, match = "A idade não pode ser negativa!"):
+        Pessoa("Fuboca", -23, Sexo.MASCULINO)
+
+def test_pessoa_idade_maior_130_mostrar_mensagem():  
+    with pytest.raises(ValueError, match = "A idade não pode ser maior que 130 anos!"):
+        Pessoa("Fuboca", 135, Sexo.MASCULINO)
+
+
     
-def test_pessoa_idade_negativa(pessoa_valida):  
-    pessoa_valida._set_idade(-1)
-    assert pessoa_valida._idade == 0
